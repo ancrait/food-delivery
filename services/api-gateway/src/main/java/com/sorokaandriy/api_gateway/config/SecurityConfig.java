c98af24e41f5c3d4255c6e5163827b7f39e90587
ancrait <xdxddclash@gmail.com>
2026-08-01 22:10:28 +0300

    add SecurityConfig to service and add mapping for api gateway and change endpoint in api gateway

--- services/api-gateway/src/main/java/com/sorokaandriy/api_gateway/config/SecurityConfig.java
+++ services/api-gateway/src/main/java/com/sorokaandriy/api_gateway/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                         .pathMatchers("/api/v1/auth/**", "/actuator/**").permitAll()
                         .pathMatchers(HttpMethod.GET).permitAll()
                         .pathMatchers("/api/v1/restaurants/**", "/api/v1/categories/**",
-                                "/api/v1/orders/**").hasAnyRole("CUSTOMER","ADMIN")
+                                "/api/v1/orders/**","/api/v1/deliveries/**")
+                        .hasAnyRole("CUSTOMER","ADMIN","RIDER")
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
