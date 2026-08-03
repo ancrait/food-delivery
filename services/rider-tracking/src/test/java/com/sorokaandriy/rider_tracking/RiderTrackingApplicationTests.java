96274626b5a6bbedb7ae8144758b50fe4778d3e9
ancrait <xdxddclash@gmail.com>
2026-08-03 21:40:43 +0300

    init rider-tracking service

---
+++ services/rider-tracking/src/test/java/com/sorokaandriy/rider_tracking/RiderTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorokaandriy.rider_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
